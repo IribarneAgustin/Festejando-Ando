7b1609337f11d40db1a1be077dc4877b390cb3a4
Agustin Iribarne <airibarne@luminaamericas.com>
2023-05-14 23:13:46 -0300

    init repo

---
+++ src/main/java/com/microservice/festejandoando/FestejandoandoApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.festejandoando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestejandoandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestejandoandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/festejandoando/FestejandoandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.festejandoando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestejandoandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
