3a42d13083216bf2af030398b6abbb55d46575aa
Agustin Iribarne <airibarne@luminaamericas.com>
2023-10-01 16:31:12 -0300

    scheduled task disabled

--- src/main/java/com/microservice/festejandoando/FestejandoandoApplication.java
+++ src/main/java/com/microservice/festejandoando/FestejandoandoApplication.java
@@ -2,10 +2,8 @@ package com.microservice.festejandoando;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class FestejandoandoApplication {
 
 	public static void main(String[] args) {
